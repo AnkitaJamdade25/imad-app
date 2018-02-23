103036018e9ad1d250dbdabebe3a2a590cebda6f
AnkitaJamdade25 <36453931+AnkitaJamdade25@users.noreply.github.com>
2018-02-23 09:47:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,15 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+var names=[];
+app.get('/submit-name', function (req, res) {
+  var name = request.query.name;
+  
+  names.push(name);
+  
+  res.send(JSON.stringify(names));
+});
+
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
