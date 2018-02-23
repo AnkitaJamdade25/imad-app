e198906298918378a2da9cef059795104e8724f4
AnkitaJamdade25 <36453931+AnkitaJamdade25@users.noreply.github.com>
2018-02-23 09:03:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,28 @@ console.log('Loaded!');
 
 var btn = document.getElementById('button');
 btn.onclick = function(){
-    alert('clicked');
-}
+    //alert('clicked');
+    
+    //Create a request
+    var request = new XMLHttpRequest();
+    
+    
+    request.onreadystatechange = function(){
+        
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status === 200){
+                
+                //Take some action
+                var counter = request.responseText;
+                var changeText = document.getElementById('count');
+                changeText.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    
+    //Make a request
+    reuqest.open('GET','http://ankitajamdade25.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
+};
