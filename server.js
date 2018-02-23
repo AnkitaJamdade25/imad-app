cb6b4b1659f3d0fb1e9bf7f33a9b6b07e0f41eb8
AnkitaJamdade25 <36453931+AnkitaJamdade25@users.noreply.github.com>
2018-02-23 07:56:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.jsg'));
+});
+
+
 
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
